e5c0150bb1f22375be5b76754a09e61a615777c2
根岸 渉 <teteruru3690@gmail.com>
2019-05-27 16:17:05 +0900

    熊の画像を追加

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -101,20 +101,30 @@ bool HelloWorld::init()
         this->addChild(label, 1);
     }
 
-    // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
-    if (sprite == nullptr)
-    {
-        problemLoading("'HelloWorld.png'");
-    }
-    else
-    {
-        // position the sprite on the center of the screen
-        sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+#pragma region いらない場所
 
-        // add the sprite as a child to this layer
-        this->addChild(sprite, 0);
-    }
+
+    // add "HelloWorld" splash screen"
+    //auto sprite = Sprite::create("HelloWorld.png");
+    //if (sprite == nullptr)
+    //{
+    //    problemLoading("'HelloWorld.png'");
+    //}
+    //else
+    //{
+    //    // position the sprite on the center of the screen
+    //    sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+
+    //    // add the sprite as a child to this layer
+    //    this->addChild(sprite, 0);
+    //}
+#pragma endregion
+
+	// テクスチャファイル名を指定して、スプライトを作成
+	Sprite* sprite = Sprite::create("kuma.png");
+	// シーングラフにつなぐ
+	this->addChild(sprite);
+
     return true;
 }
 
